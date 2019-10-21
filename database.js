5afdbd0d405fb797d5c7a0b7f97572bb02518bb1
John Wregglesworth <wregglej@iplantcollaborative.org>
2019-10-21 13:09:04 -0700

    Connect with DE_DB_URL env var containing connection string

--- database.js
+++ database.js
@@ -1,7 +1,9 @@
 const { Pool } = require('pg');
 
 // Uses the environment variabled supported by 'pg' to configure the pool.
-const pool = new Pool();
+const pool = new Pool({
+    connectionString: process.env.DE_DB_URL,
+});
 
 module.exports = {
     query: (text, params) => {
